dd4df0e927cc7e4b50125f2d0b045cac13d029c4
va-14 <vince1@live.nl>
2015-11-10 18:33:24 +0100

    naamgeving iets veranderd

--- Subby/Subby/Level.cs
+++ Subby/Subby/Level.cs
@@ -266,21 +266,21 @@ namespace Subby
             int heightRadius = Subby.Texture.Height / 2;
             List<Rectangle> values = new List<Rectangle>();
 
-            for (int x = 0; x < widthRadius; x = x + pixels)
+            for (int tmpWidthRadius = 0; tmpWidthRadius < widthRadius; tmpWidthRadius = tmpWidthRadius + pixels)
             {
-                for (int y = 0; y < heightRadius; y = y + pixels)
+                for (int tmpHeightRadius = 0; tmpHeightRadius < heightRadius; tmpHeightRadius = tmpHeightRadius + pixels)
                 {
-                    Point radiusPoint = PointOnCircle(x, (int)Subby.AngleDegrees, new Point((int)Subby.Position.X, (int)Subby.Position.Y));
-                    Point point = PointOnCircle(y, (int)Subby.AngleDegrees - 90, new Point((int)radiusPoint.X, (int)radiusPoint.Y));
+                    Point radiusPoint = PointOnCircle(tmpWidthRadius, (int)Subby.AngleDegrees, new Point((int)Subby.Position.X, (int)Subby.Position.Y));
+                    Point point = PointOnCircle(tmpHeightRadius, (int)Subby.AngleDegrees - 90, new Point((int)radiusPoint.X, (int)radiusPoint.Y));
                     values.Add(new Rectangle(point, size));
-                    radiusPoint = PointOnCircle(x, (int)Subby.AngleDegrees, new Point((int)Subby.Position.X, (int)Subby.Position.Y));
-                    point = PointOnCircle(y, (int)Subby.AngleDegrees + 90, new Point((int)radiusPoint.X, (int)radiusPoint.Y));
+                    radiusPoint = PointOnCircle(tmpWidthRadius, (int)Subby.AngleDegrees, new Point((int)Subby.Position.X, (int)Subby.Position.Y));
+                    point = PointOnCircle(tmpHeightRadius, (int)Subby.AngleDegrees + 90, new Point((int)radiusPoint.X, (int)radiusPoint.Y));
                     values.Add(new Rectangle(point, size));
-                    radiusPoint = PointOnCircle(-x, (int)Subby.AngleDegrees, new Point((int)Subby.Position.X, (int)Subby.Position.Y));
-                    point = PointOnCircle(y, (int)Subby.AngleDegrees - 90, new Point((int)radiusPoint.X, (int)radiusPoint.Y));
+                    radiusPoint = PointOnCircle(-tmpWidthRadius, (int)Subby.AngleDegrees, new Point((int)Subby.Position.X, (int)Subby.Position.Y));
+                    point = PointOnCircle(tmpHeightRadius, (int)Subby.AngleDegrees - 90, new Point((int)radiusPoint.X, (int)radiusPoint.Y));
                     values.Add(new Rectangle(point, size));
-                    radiusPoint = PointOnCircle(-x, (int)Subby.AngleDegrees, new Point((int)Subby.Position.X, (int)Subby.Position.Y));
-                    point = PointOnCircle(y, (int)Subby.AngleDegrees + 90, new Point((int)radiusPoint.X, (int)radiusPoint.Y));
+                    radiusPoint = PointOnCircle(-tmpWidthRadius, (int)Subby.AngleDegrees, new Point((int)Subby.Position.X, (int)Subby.Position.Y));
+                    point = PointOnCircle(tmpHeightRadius, (int)Subby.AngleDegrees + 90, new Point((int)radiusPoint.X, (int)radiusPoint.Y));
                     values.Add(new Rectangle(point, size));
                 }
             }
@@ -298,19 +298,15 @@ namespace Subby
 
             int boundingLength;
             if (Subby.Width > Subby.Height)
-            {
                 boundingLength = Subby.Width;
-            }
             else
-            {
                 boundingLength = Subby.Height;
-            }
+           
             Rectangle rectSubby = new Rectangle((int)Subby.Position.X - (boundingLength / 2), (int)Subby.Position.Y - (boundingLength / 2), boundingLength, boundingLength);
             Boolean subbyCollision;
             foreach (ISprite sprite in SpriteList)
             {
                 subbyCollision = false;
-                //collision voor subby is apart, want deze wordt opgedeeld in meerdere vierkantjes
                 Rectangle rectSprite = new Rectangle((int)sprite.Position.X - ScrollingPosition, (int)sprite.Position.Y, sprite.Width, sprite.Height);
                 Rectangle overlap = Rectangle.Intersect(rectSubby, rectSprite);
                 if (!overlap.IsEmpty)
